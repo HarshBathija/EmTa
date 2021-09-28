66b009155d4e77464bb59fafa018093e45fc9120
Aditya Jadhav <adityajadhav@Adityas-Air.lan>
2021-09-28 11:04:22 -0400

    Added CORS Filter

--- src/main/java/ncsu/se/backend/controller/StudentController.java
+++ src/main/java/ncsu/se/backend/controller/StudentController.java
@@ -3,6 +3,7 @@ package ncsu.se.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,24 +26,23 @@ public class StudentController {
 	@Autowired
 	private StudentRepository studentrepo;
 
+	@CrossOrigin
 	@GetMapping("/studentDetails")
 	public List<StudentDetails> getStudentDetails(@RequestParam int sid) {
 		return studentService.getStudentDetails(sid);
 	}
 
+	@CrossOrigin
 	@PostMapping("/updateMealRecord")
 	public String updateMealRecord(@RequestBody MealRecords mr) {
 		return studentService.updateMealRecords(mr);
 	}
 
+	@CrossOrigin
 	@GetMapping("/getDefaultersList")
 	public String getDefaultersList() {
 		studentrepo.generateDefaultersStudents();
 		return "generated successfully";
 	}
-}
 
-// @GetMapping("/addClockIn")
-// public String addClockInTime(@RequestParam int sid){
-// return studentService.addClockInTime(sid);
-// }
+}
