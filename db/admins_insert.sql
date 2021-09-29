bc5f2b73eeff7af7b70c83cf4129b40addba0cfc
akash-sanghani <akashsanghani@hotmail.com>
2021-09-29 18:58:04 -0400

    Insert queries for data

---
+++ db/admins_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO admins(user_id, email_id, pass)
+VALUES ('clarissa_hopkins', 'chopkins@ncsu.edu', '*********');
+
+INSERT INTO admins(user_id, email_id, pass)
+VALUES ('monica', 'monica123@ncsu.edu', '*********');
+
+INSERT INTO admins(user_id, email_id, pass)
+VALUES ('angela_wang', 'awang143@ncsu.edu', '*********');
+
+INSERT INTO admins(user_id, email_id, pass)
+VALUES ('tiffany_james', 'tjames1234@ncsu.edu', '*********');
---
+++ db/meal_info_insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200099333', '4.49', '2021-09-20 19:10:29', 'Tuffys Diner');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200099333', '4.49', '2021-09-21 19:10:29', 'Jasons Deli');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200066333', '7.49', '2021-09-20 12:16:29', 'Jasons Deli');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200066333', '7.56', '2021-09-22 10:17:29', 'Starbucks');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200066333', '3.49', '2021-09-23 12:16:29', 'Tuffys Diner');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200066444', '6.19', '2021-09-18 17:16:30', 'Starbucks');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200066444', '5.11', '2021-09-20 15:45:30', 'Jasons Deli');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200099555', '5.15', '2021-09-19 09:45:07', 'Port City Java');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200099555', '5.75', '2021-09-21 11:45:56', 'Port City Java');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200066555', '5.15', '2021-09-24 09:55:39', 'Starbucks');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200088444', '4.49', '2021-09-20 20:10:29', 'Tuffys Diner');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200088555', '4.49', '2021-09-21 19:10:29', 'Jasons Deli');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200088555', '7.49', '2021-09-20 12:16:29', 'Starbucks');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200055555', '7.19', '2021-09-21 13:16:29', 'Starbucks');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200055555', '7.49', '2021-09-22 12:19:29', 'Starbucks');
+
+INSERT INTO meal_info(id, meal_cost, meal_timestamp, meal_store)
+VALUES ('200055555', '7.39', '2021-09-24 11:56:57', 'Jasons Deli');
